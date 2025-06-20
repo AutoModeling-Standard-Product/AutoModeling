b100fa19dad91324c159e6afde2e0d6e78a741da
Renge3 <92766932+Renge3@users.noreply.github.com>
2025-06-20 16:59:11 +0800

    Update data_augmentation.py
        # 简化如下：
        # 1. 直接调用采样器，不再分别传入采样器类型和参数。要确保传入的采样器及参数符合过采样要求；
        # 2. 去掉“是否进行机构均衡”开关，直接对各样本机构分别重采样，否则直接对整体重采样时无法分配机构id；
        # 3. 直接在总体样本索引最大值之上构建新索引，并按机构更新。
    
        # 目前待解决的问题：
        # 1. 少数类节点与邻居节点默认以线性变换为融合方式，新节点可能会出现异常值（如整数特征生成浮点数插值，或是填充的缺省值被学习到）
        # 2. 填充值可能会影响邻居节点的欧氏距离计算
        # 3. 生成的新节点无法模拟或还原缺失值
        # 待验证手动方案：对标准化后的特征进行k临近寻找->投票决定特征值->归一化特征还原

--- data_augmentation.py
+++ data_augmentation.py
@@ -18,76 +18,47 @@ from scipy import special, optimize
 from scipy.misc import derivative
 
 
-def resampling(
-    X_tot: pd.DataFrame,
-    y_tot: pd.DataFrame,
-    algo: str,
-    params: dict,
-    label: str = 'new_target',
-    org_tag: str = 'new_org',
-    org_balance: bool = False
+def over_sampling(
+    sampler，
+    X_tr: pd.DataFrame,
+    y_tr: pd.DataFrame,
+    tr_orgidx: dict,
+    start_index: int,
+    fill_value: int = -999
 ):
     """
-    :param X_tot          切分后的训练集自变量特征，需要包含org_tag
-    :param y_tot          切分后的训练集因变量。注意，由于需要包含label和org_tag两部分，这里是DataFrame而非Series
-    :param algo           选择采样器类型
-    :param params         采样器对应参数字典，一般的，sampling_strategy和random_state是必选指定项
-    :param label          因变量标识
-    :param org_tag。      机构名称标识，必须存在于X_tot和y_tot中以便采样
-    :param org_balance    是否对每家机构单独重采样，以保持机构间坏样率的一致性
+    :param sampler       选择采样器，需要一次性定义算法及参数，传入范例：SMOTE(sampling_strategy='auto', random_state=42, k_neighbors=5)
+    :param X_tr          切分后的训练集特征
+    :param y_tr          切分后的训练集目标变量
+    :param tr_orgidx     训练集样本对应的机构id
+    :param start_index   训练集重新生成索引的起始点，建议设置为当前整体dataframe索引最大值+1
+    :param fill_value    imblearn大部分重采样算法均需要预先填充缺失值，以保证k临近搜索可以正确执行
+
+    返回过采样之后的X_tr，y_tr，tr_orgidx
     """
-
-    def resampler_selection(X: pd.DataFrame, y: pd.DataFrame, algo: str, params: dict):
-        try:
-            assert algo in [
-                'RandomOverSampler', 'ADASYN', 'SMOTE', 'SMOTENC', 'BorderlineSMOTE', 'KMeansSMOTE', 'SVMSMOTE',
-                'RandomUnderSampler', 'TomekLinks', 'ClusterCentroids'
-            ]
-            if algo == 'ADASYN':
-                sampler = ADASYN(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'], n_neighbors=['n_neighbors'])
-            elif algo == 'RandomOverSampler':
-                sampler = RandomOverSampler(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'SMOTE':
-                sampler = SMOTE(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])          
-            elif algo == 'SMOTENC':
-                sampler = SMOTENC(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'BorderlineSMOTE':
-                sampler = BorderlineSMOTE(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'KMeansSMOTE':
-                sampler = KMeansSMOTE(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])            
-            elif algo == 'SVMSMOTE':
-                sampler = SVMSMOTE(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'RandomUnderSampler':
-                sampler = RandomUnderSampler(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'TomekLinks':
-                sampler = TomekLinks(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            elif algo == 'ClusterCentroids':
-                sampler = ClusterCentroids(sampling_strategy=params['sampling_strategy'], random_state=params['random_state'])
-            
-            return sampler.fit_resample(X, y)
-
-        except Exception as e:
-            display(f'采样器运作失败：{e}')
-
+    
     try:
-        if not org_balance:
-            return resampler_selection(X_tot.drop(columns=[org_tag]), y_tot[label], algo, params)
-        else:
-            if org_tag in X_tot.columns and org_tag in y_tot.columns:
-                result_X, result_y = [], []
-                for org_name in tqdm(X_tot[org_tag].unique()):
-                    X_org = X_tot[X_tot[org_tag] == org_name].drop(columns=[org_tag])
-                    y_org = y_tot[y_tot[org_tag] == org_name].drop(columns=[org_tag])    
-                    X_resampled, y_resampled = resampler_selection(X_org, y_org, algo, params)
-                    result_X.append(X_resampled)
-                    result_y.append(y_resampled)
-                return np.vstack(X_resampled), np.concatenate(result_y)
-            else:
-                display('X或y的机构名称缺失，无法分机构重采样')
-                return
-    except Exception as e:
-            display(f'重采样失败：{e}')
+        result_X, result_y = [], []
+        result_index = {org: [] for org in tr_orgidx.keys()}
+        start_index_ = start_index
+        for org_name in tqdm(tr_orgidx.keys()):
+            result_X_org, result_y_org = sampler.fit_resample(
+                X_tr.loc[tr_orgidx[org]].fillna(fill_value), # 这里需要做缺失值填充
+                y_tr.loc[tr_orgidx[org]]
+            )
+            result_index_org = np.arange(start_index_, start_index_ + len(result_X_org))
+            result_X_org = pd.DataFrame(result_X_org, columns=X_tr.columns, index=result_index_org)
+            result_y_org = pd.Series(result_y_org, index=result_index_org)
 
+            result_X.append(result_X_org)
+            result_y.append(result_y_org)
+            result_index[org] = result_index_org
+            start_index_ += len(result_X_org)
+            
+        return pd.concat(result_X).replace(fill_value, np.nan), pd.concat(result_y), result_index
+    except Exception as e:
+        display(f'过采样失败：{e}')
+        return
 
 
 def re_weight_by_org(y_tr: pd.Series, tr_orgidx: dict, scale: float = 1.0, broadcast_with_tar: bool = False):
